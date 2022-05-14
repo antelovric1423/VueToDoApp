b61f0f173e0abfd3663427c717e24ab72f986ba6
Ante Lovric <ante.lovric77@gmail.com>
2022-05-14 18:51:34 +0200

    Add task list, add and remove task functionality (frontend only)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,115 @@
 <template>
-  <h1>Hello world!</h1>
+  <div class="container">
+    <Header @toggle-add-task="toggleAddTask()" title="Task Tracker" :showingAddTask="showAddTask" />
+    <div v-if="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue"
+import Tasks from "./components/Tasks.vue"
+import AddTask from "./components/AddTask.vue"
 
 export default {
   name: 'App',
   components: {
-
+    Header,
+    Tasks,
+    AddTask
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    }
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task]
+    },
+    deleteTask(id) {
+      console.log('deleting task:', id)
+      if (confirm("Are you sure you want to delete this task?"))
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+    },
+    toggleReminder(id) {
+      console.log('toggling reminder for task:', id)
+      this.tasks = this.tasks.map((task) => { if (task.id === id) task.reminder = !task.reminder; return task; })
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Preacher appointment",
+        time: "May 14th at 11:30",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Preacher appointment",
+        time: "May 21st at 09:00",
+        reminder: true,
+      },
+    ]
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @button-clicked="$emit('toggle-add-task')" :text="showingAddTask ? 'Close' : 'Add Task'"
+            :color="showingAddTask ? 'red' : 'green'" />
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue"
+export default {
+    name: "Header",
+    props: {
+        title: String,
+        showingAddTask: Boolean
+    },
+    components: { Button }
+}
+</script>
+
+<style>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
